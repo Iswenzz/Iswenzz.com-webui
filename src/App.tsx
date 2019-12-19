c6778ab63b1bbab12d5781745488cf8eb3d7c7a3
iswenzz <suxlolz1528@gmail.com>
2019-12-19 23:38:05 +0100

    App: added custom palette (MuiTheme)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import NavBar from './containers/NavBar/NavBar';
+
+import { Theme, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 class App extends Component
 {
 	render(): JSX.Element
 	{
+		const theme: Theme = createMuiTheme({
+			palette: {
+				primary: { // Discord hex
+					light: '#99aab5',
+					main: '#2c2f33',
+					dark: '#23272a',
+					contrastText: '#dcdcdc',
+				},
+			},
+		});
+
 		return (
-			<div className="App">
-				<h1>Hello World !</h1>
-			</div>
+			<ThemeProvider theme={theme}>
+				<NavBar />
+			</ThemeProvider>
 		);
 	}
 }
