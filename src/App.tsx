b0c46546189f47a59b6d3aa89f3155e9282b576c
iswenzz <suxlolz1528@gmail.com>
2019-12-20 15:47:05 +0100

    App: added a RadialGradient at the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './containers/NavBar/NavBar';
+import RadialGradient from './components/RadialGradient/RadialGradient';
 import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
 
 import { Theme, createMuiTheme } from '@material-ui/core/styles';
@@ -24,10 +25,12 @@ class App extends Component
 			<ParallaxProvider>
 				<ThemeProvider theme={theme}>
 					<NavBar />
-					<ParallaxBanner 
-					layers={[{ image: 'https://iswenzz.com/assets/images/medieval_village/3.jpg', 
-						amount: 0.4, children: null}]} style={{height: '400px'}}>
-					</ParallaxBanner>
+					<RadialGradient height='400px' position='ellipse at bottom' colors={[
+						{ color: '#23272a', colorPercent: '0%' },
+						{ color: '#090A0F', colorPercent: '100%' }
+					]} />
+					<ParallaxBanner layers={[{ image: 'https://iswenzz.com/assets/images/medieval_village/3.jpg', 
+						amount: 0.4, children: null}]} style={{height: '400px'}} />
 
 					{/* Scroll Test */}
 					<div style={{ height: '1200px' }} />
