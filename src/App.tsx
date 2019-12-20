ca432cde4d2148fe5b03d954dfe2cc1ca5cc8365
iswenzz <suxlolz1528@gmail.com>
2019-12-20 19:16:28 +0100

    App: Intro text with parallax bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax } from 'react-parallax';
+import { Theme, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+
 import NavBar from './containers/NavBar/NavBar';
 import RadialGradient from './components/RadialGradient/RadialGradient';
-import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
-
-import { Theme, createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/core/styles';
+import Footer from './containers/Footer/Footer';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
 class App extends Component
 {
 	render(): JSX.Element
 	{
-		const theme: Theme = createMuiTheme({
+		let theme: Theme = createMuiTheme({
 			palette: {
 				primary: { // Discord hex
 					light: '#99aab5',
@@ -20,20 +24,39 @@ class App extends Component
 				},
 			},
 		});
+		theme = responsiveFontSizes(theme);
 
 		return (
 			<ParallaxProvider>
-				<ThemeProvider theme={theme}>
+				<ThemeProvider theme={theme}> 
 					<NavBar />
-					<RadialGradient height='400px' position='ellipse at bottom' colors={[
-						{ color: '#23272a', colorPercent: '0%' },
-						{ color: '#090A0F', colorPercent: '100%' }
+					{/* Intro */}
+					<Parallax bgImage={require('./assets/images/index/1.jpg')} bgImageAlt="index bg1" strength={500}>
+						<div style={{ height: '200px' }} />
+						<Grid container direction="row" justify="center" alignItems="center">
+							<Typography style={{fontFamily: "Exo, sans-serif", fontWeight: 1000,
+							color: 'rgba(220, 220, 220, 0.9)'}} align="center" variant="h3" component="h2">
+								<div style={{fontFamily: "'Press Start 2P', cursive", fontSize: 60}}>Iswenzz</div><br />
+								Software Engineer and Level Designer
+							</Typography>
+						</Grid>
+						<div style={{ height: '200px' }} />
+					</Parallax>
+					{/* About */}
+					<RadialGradient height='600px' position='ellipse at bottom' colors={[
+						{ color: '#23272B', colorPercent: '0%' },
+						{ color: '#090A0A', colorPercent: '100%' }
 					]} />
-					<ParallaxBanner layers={[{ image: 'https://iswenzz.com/assets/images/medieval_village/3.jpg', 
-						amount: 0.4, children: null}]} style={{height: '400px'}} />
-
-					{/* Scroll Test */}
-					<div style={{ height: '1200px' }} />
+					<Parallax bgImage={require('./assets/images/index/2.jpg')} bgImageAlt="index bg2" strength={500}>
+						<div style={{ height: '500px' }} />
+					</Parallax>
+					{/* Projects */}
+					<RadialGradient height='600px' position='ellipse at bottom' colors={[
+						{ color: '#23272B', colorPercent: '0%' },
+						{ color: '#090A0A', colorPercent: '100%' }
+					]} />
+					{/* Level Design */}
+					<Footer />
 				</ThemeProvider>
 			</ParallaxProvider>
 		);
