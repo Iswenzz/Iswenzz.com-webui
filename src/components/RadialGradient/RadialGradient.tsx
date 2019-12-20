70b7e3ebfa6f09d882ed13a11a8d5e08cf7dbaeb
iswenzz <suxlolz1528@gmail.com>
2019-12-20 15:46:48 +0100

    RadialGradient: Component to draw a gradient background

---
+++ src/components/RadialGradient/RadialGradient.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+interface IGradientColor
+{
+    color: string,
+    colorPercent: string
+}
+
+interface IGradient
+{
+    height?: string,
+    position?: string,
+    colors?: IGradientColor[]
+}
+
+class RadialGradient extends Component<IGradient>
+{
+    processBackgroundColor = (): string =>
+    {
+        let colors: string = '';
+        this.props.colors?.forEach((c, index, arr) => 
+            colors += `${c.color} ${c.colorPercent} ${(index + 1 !== arr.length) ? ',' : ''}`);
+        return `radial-gradient(${this.props.position}, ${colors})`;
+    }
+
+    render() : JSX.Element
+    {
+        return (
+            <div style={{ height: this.props.height, background: this.processBackgroundColor() }} />
+        );
+    }
+}
+
+export default RadialGradient;
