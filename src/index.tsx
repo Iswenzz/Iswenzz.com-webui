717a5d23c677481d24c97d51f53ed4d4fb1f8aae
iswenzz <suxlolz1528@gmail.com>
2019-12-22 18:41:30 +0100

    Index: Added react-redux and thunk middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import homeReducer from './containers/Home/store/reducer';
+const composeEnhancers: any = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    home: homeReducer
+});
+export type AppState = ReturnType<typeof rootReducer>;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+const app: JSX.Element = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
