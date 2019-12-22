527c7d5dac6547a19f94ef743d344ea66085e715
iswenzz <suxlolz1528@gmail.com>
2019-12-22 22:07:52 +0100

    TrailText: use props instead of useState

--- src/components/TrailText/TrailText.tsx
+++ src/components/TrailText/TrailText.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 import { useTrail, animated } from 'react-spring';
 
 export interface TrailProps
@@ -11,12 +11,11 @@ export interface TrailProps
 const config = { mass: 5, tension: 2000, friction: 200 }
 const TrailText: FunctionComponent<TrailProps> = (props: TrailProps): JSX.Element =>
 {
-	const [toggle] = useState(!props.active!);
 	const trail = useTrail(props.items.length, {
 		config,
-		opacity: toggle ? 1 : 0,
-		x: toggle ? 0 : 20,
-		height: toggle ? 80 : 0,
+		opacity: props.active ? 1 : 0,
+		x: props.active ? 0 : 20,
+		height: props.active ? 80 : 0,
 		from: { opacity: 0, x: 20, height: 0 },
 	});
 
