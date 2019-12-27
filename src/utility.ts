64ad08619cc8a15ced36bdc0bea07d2f744a8e1e
iswenzz <suxlolz1528@gmail.com>
2019-12-27 20:52:14 +0100

    utility.js: shuffleArray func

--- src/utility.ts
+++ src/utility.ts
@@ -4,3 +4,14 @@ export const updateObject = (oldObject: Object, updatedProperties: Object): any 
         ...updatedProperties
     };
 };
+
+export function shuffleArray(array: any[]): void
+{
+    for (let i = array.length - 1; i > 0; i--) 
+    {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
