c4c5fc8d407844c3ce8240415525951fdc90185b
iswenzz <suxlolz1528@gmail.com>
2020-01-16 00:03:35 +0100

    ProjectPopup: responsive container

--- src/containers/Home/UI/ProjectPopup/ProjectPopup.tsx
+++ src/containers/Home/UI/ProjectPopup/ProjectPopup.tsx
@@ -1,16 +1,17 @@
 import * as actions from '../../store/actions';
-import React, { FunctionComponent, useEffect } from "react";
-import { Dialog, DialogContent, Slide, Fab, Grid } from "@material-ui/core";
-import { TransitionProps } from "@material-ui/core/transitions/transition";
+import React, { FunctionComponent, useEffect, useRef } from "react";
+import { DialogContent, Fab, Grid, Modal, Fade, Backdrop } from "@material-ui/core";
 import { Close, Lock } from '@material-ui/icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faOsi } from '@fortawesome/free-brands-svg-icons';
 import { LinkedProjectProps } from "../Project/Project";
-import ViewPager from '../../../../components/ViewPager/ViewPager';
+import ViewPager, { ViewPagerConfig } from '../../../../components/ViewPager/ViewPager';
 import { AppState } from "../../../..";
 import marked from "marked";
+import useWindowSize from '../../../../Utility/useWindowSize';
 import { useSelector, useDispatch } from "react-redux";
 import RadialGradient from '../../../../components/RadialGradient/RadialGradient';
+import { useMediaQuery } from 'react-responsive';
 import './ProjectPopup.scss';
 
 export interface ProjectPopupProps
@@ -19,13 +20,12 @@ export interface ProjectPopupProps
 	children?: any
 }
 
-const Transition = React.forwardRef<unknown, TransitionProps>(function Transition(props, ref) 
-{
-	return <Slide direction="up" ref={ref} {...props} />;
-});
-
 const ProjectPopup: FunctionComponent<ProjectPopupProps> = (props: ProjectPopupProps): JSX.Element =>
 {
+	const isPortrait = useMediaQuery({ orientation: 'portrait' });
+	const [width, height] = useWindowSize();
+	const viewPagerRef = useRef<HTMLDivElement>(null);
+
 	const projectModalActive = useSelector((state: AppState) => state.home.projectModalActive);
 	const projectsStartIndex = useSelector((state: AppState) => state.home.projectsStartIndex);
 	const dispatch = useDispatch();
@@ -65,7 +65,7 @@ const ProjectPopup: FunctionComponent<ProjectPopupProps> = (props: ProjectPopupP
 				<Lock />
 			</Fab>
 		);
-
+		
 		return (
 			<div>
 				{/* Modal Navbar */}
@@ -84,16 +84,16 @@ const ProjectPopup: FunctionComponent<ProjectPopupProps> = (props: ProjectPopupP
 							<Close />
 						</Fab>
 					</div>
-				</Grid>
+				</Grid> 
 
 				{/* Modal Content */}
-				<RadialGradient position='ellipse at bottom' colors={[
+				<RadialGradient container position='ellipse at bottom' colors={[
 				{ color: '#323536', colorPercent: '0%' },
 				{ color: '#222428', colorPercent: '100%' }]}>
 					<DialogContent className='project-modal'>
 						{project.showTitle ? project.title : null}
 						{project.desc}
-						<div className='project-md' id={`popupProjectMd-${project.title}`} />
+						<div className="project-md" id={`popupProjectMd-${project.title}`} /> 
 						{props.children}
 					</DialogContent>
 				</RadialGradient>
@@ -101,13 +101,33 @@ const ProjectPopup: FunctionComponent<ProjectPopupProps> = (props: ProjectPopupP
 		);
 	}
 
+	const getConfig = (): ViewPagerConfig => 
+	{
+		const responsiveRes = {
+			height: height / 1.3,
+			width: width / 1.5
+		}
+		if (isPortrait) return {
+			height: height,
+			width: width,
+		}
+		else return {
+			height: responsiveRes.height,
+			width: responsiveRes.width,
+			top: (height / 2) - responsiveRes.height / 2,
+			right: (width / 2) - responsiveRes.width / 2
+		}
+	}
+
 	return (
-		<Dialog maxWidth='xl' open={projectModalActive} TransitionComponent={Transition} 
-		keepMounted onClose={onClickClose} aria-labelledby="alert-dialog-slide-title" 
-		aria-describedby="alert-dialog-slide-description">
-			<ViewPager startIndex={projectsStartIndex} width='1100px' height='600px' 
-			items={props.projects.map((proj: LinkedProjectProps, i: number): JSX.Element => renderProject(proj, i))} />
-		</Dialog>
+		<Modal aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description" open={projectModalActive} 
+		keepMounted onClose={onClickClose} closeAfterTransition BackdropComponent={Backdrop} 
+		BackdropProps={{ timeout: 500 }}>
+			<Fade in={projectModalActive}>
+				<ViewPager bgcolor='#202326' ref={viewPagerRef} startIndex={projectsStartIndex} config={{...getConfig()}}
+				items={props.projects.map((proj: LinkedProjectProps, i: number): JSX.Element => renderProject(proj, i))} />
+			</Fade>
+		</Modal>
 	);
 }
 
