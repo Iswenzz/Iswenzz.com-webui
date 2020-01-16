091112c3addb6ee52a1389467d589363b53fefb7
iswenzz <suxlolz1528@gmail.com>
2020-01-17 00:57:37 +0100

    App: dark/light mode default properties

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,34 @@ import { Theme, createMuiTheme, responsiveFontSizes } from '@material-ui/core/st
 import { ThemeProvider } from '@material-ui/core/styles';
 import Home from './containers/Home/Home';
 
+const isDarkMode: boolean = true;
+
 let theme: Theme = createMuiTheme({
+	overrides: {
+		MuiTooltip: {
+			tooltip: {
+			  fontSize: "1em",
+			}
+		}
+	},
 	palette: {
-		primary: { // Discord hex
+		type: isDarkMode ? 'dark' : 'light',
+		text: {
+			primary: `rgba(${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, 0.87)`,
+			secondary: `rgba(${isDarkMode ? 220 : 60},${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, 0.54)`,
+			disabled: `rgba(${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, 0.38)`,
+			hint: `rgba(${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, ${isDarkMode ? 220 : 60}, 0.38)`,
+		},
+		primary: {
 			light: '#99aab5',
 			main: '#2c2f33',
 			dark: '#23272a',
+			contrastText: '#dcdcdc',
+		},
+		secondary: {
+			light: '#ff4081',
+			main: '#f50057',
+			dark: '#c51162',
 			contrastText: '#dcdcdc',
 		},
 	},
