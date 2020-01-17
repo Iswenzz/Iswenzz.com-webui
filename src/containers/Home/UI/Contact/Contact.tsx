552ac51340827f8540f80dc8db6ac54f52b0c21a
iswenzz <suxlolz1528@gmail.com>
2020-01-17 01:00:20 +0100

    Contact: added form & text fields

--- src/containers/Home/UI/Contact/Contact.tsx
+++ src/containers/Home/UI/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import RadialGradient from '../../../../components/RadialGradient/RadialGradient';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Container, Avatar, TextField, Button, makeStyles } from '@material-ui/core';
 import Spacing from '../../../../components/Spacing/Spacing';
 import '../../../../Text.scss';
 import SplitText from 'react-pose-text';
@@ -14,8 +14,31 @@ const charPoses = {
     }
 };
 
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        width: '80px',
+        height: '80px'
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
 const Contact: FunctionComponent = (): JSX.Element =>
 {
+    const classes = useStyles();
+
     return (
         <Grid container direction="column" justify="center" alignItems="center">
 
@@ -30,14 +53,33 @@ const Contact: FunctionComponent = (): JSX.Element =>
 
             <Spacing height='200px' />
 
-            {/* Projects */}
-            <div style={{width: '100%', height: '100%', listStyleType: 'none'}}>
-                <RadialGradient position='ellipse at bottom' colors={[
-                { color: '#23272B', colorPercent: '0%' },
-                { color: '#090A0A', colorPercent: '100%' }]}>
-                    <Spacing height='600px' />
-                </RadialGradient>
-            </div>
+            {/* Contact */}
+            <RadialGradient position='ellipse at bottom' colors={[
+            { color: '#23272B', colorPercent: '0%' },
+            { color: '#090A0A', colorPercent: '100%' }]}>
+                <Container maxWidth="md">
+                    <Grid container direction="row" justify="center" alignItems="center">
+                        <Avatar alt='iswenzz avatar' src={require('../../../../assets/images/misc/iswenzz.png')} 
+                        className={classes.avatar} />
+                        <form className={classes.form} noValidate>
+                            <TextField color="secondary" variant="outlined" margin="normal" required fullWidth id="email"
+                            label="Email Address" name="email" autoComplete="email" autoFocus />
+                            <TextField color="secondary" variant="outlined" margin="normal" required fullWidth id="subject"
+                            label="Subject" name="subject" />
+                            <TextField multiline rows="6" color="secondary" variant="outlined" 
+                            margin="normal" required fullWidth id="message" label="Message" name="message" />
+
+                            <Container maxWidth="xs">
+                                <Button fullWidth className={classes.submit} type="submit" variant="contained" 
+                                color="secondary">
+                                    Send
+                                </Button>
+                            </Container>
+                        </form>
+                    </Grid>
+                </Container>
+                <Spacing height='600px' />
+            </RadialGradient>
             
         </Grid>
     );
