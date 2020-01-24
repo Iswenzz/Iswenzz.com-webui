17db3ff08b960edfcfb02c6720cea4700926844c
iswenzz <suxlolz1528@gmail.com>
2020-01-24 23:17:51 +0100

    RadialGradient: accept config props and linear gradient option

--- src/components/RadialGradient/RadialGradient.tsx
+++ src/components/RadialGradient/RadialGradient.tsx
@@ -4,41 +4,54 @@ import { Grid, GridDirection, GridJustification, GridItemsAlignment, Box } from 
 export interface IGradientColor
 {
     color: string,
-    colorPercent: string
+    colorPercent?: string
 }
 
-export interface GradiantProps
+export interface GradiantProps extends React.HTMLAttributes<HTMLDivElement>
 {
+    config?: GradiantProps,
     container?: boolean,
-    id?: string,
+    linear?: boolean,
     direction?: GridDirection,
     justify?: GridJustification,
     alignItems?: GridItemsAlignment
     position?: string,
     colors?: IGradientColor[],
-    style?: React.CSSProperties
 }  
 
 class RadialGradient extends Component<GradiantProps>
 {
-    processBackgroundColor = (): string =>
+    processBackgroundColor = (position?: string, propsColors?: IGradientColor[], isLinear?: boolean): string =>
     {
         let colors: string = '';
-        this.props.colors?.forEach((c, index, arr) => 
-            colors += `${c.color} ${c.colorPercent} ${(index + 1 !== arr.length) ? ',' : ''}`);
-        return `radial-gradient(${this.props.position}, ${colors})`;
+        if (isLinear)
+        {
+            propsColors?.forEach((c, index, arr) => 
+                colors += `${c.color} ${(index + 1 !== arr.length) ? ',' : ''}`);
+            return `linear-gradient(${position}, ${colors})`;
+        }
+        else
+        {
+            propsColors?.forEach((c, index, arr) => 
+                colors += `${c.color} ${c.colorPercent} ${(index + 1 !== arr.length) ? ',' : ''}`);
+            return `radial-gradient(${position}, ${colors})`;
+        }
     }
 
     render() : JSX.Element
     {
-        return this.props.container ? (
-            <Box id={this.props.id} style={{ ...this.props.style, margin: '0', background: this.processBackgroundColor() }}>
+        const { container, id, justify, alignItems, direction, 
+            position, colors, linear } = this.props.config !== undefined ? this.props.config : this.props;
+            
+        return container ? (
+            <Box id={id} style={{ ...this.props.style, margin: '0', 
+            background: this.processBackgroundColor(position, colors, linear) }}>
                 {this.props.children}
             </Box>
         ) : (
-            <Grid id={this.props.id} container direction={this.props.direction || 'row'} 
-            justify={this.props.justify || 'center'} alignItems={this.props.alignItems || 'center'} 
-            style={{ ...this.props.style, background: this.processBackgroundColor() }}>
+            <Grid id={id} container direction={direction || 'row'} 
+            justify={justify || 'center'} alignItems={alignItems || 'center'} 
+            style={{ ...this.props.style, background: this.processBackgroundColor(position, colors, linear) }}>
                 {this.props.children}
             </Grid>
         );
