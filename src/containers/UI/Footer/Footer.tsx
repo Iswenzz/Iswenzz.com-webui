d9e796693116f471441f55e4fac498d0c6e0cba9
iswenzz <suxlolz1528@gmail.com>
2020-01-25 23:00:36 +0100

    Footer: removed unnecessary div

--- src/containers/UI/Footer/Footer.tsx
+++ src/containers/UI/Footer/Footer.tsx
@@ -15,20 +15,20 @@ const Footer: FunctionComponent = (): JSX.Element =>
     const isDarkMode = useSelector((state: AppState) => state.app.isDarkMode);
 
     return (
-        <div>
+        <>
             <RadialGradient position='ellipse at bottom' colors={[
             { color: isDarkMode ? '#23272A' : '#f2f2f2', colorPercent: '0%' },
             { color: isDarkMode ? '#23272F' : '#f4f4f4', colorPercent: '100%' }
             ]}>
                 <Grid container direction="row" justify="center" alignItems="center">
-                    <Fab href='https://github.com/iswenzz' color="primary" style={{ margin: 20 }} aria-label="add">
+                    <Fab href='https://github.com/iswenzz' color="primary" style={{ margin: 20 }}>
                         <FontAwesomeIcon color='silver' icon={faGithub} size='2x' />
                     </Fab>
-                    <Fab href='https://www.youtube.com/c/iswenzz' color="primary" style={{ margin: 20 }} aria-label="add">
+                    <Fab href='https://www.youtube.com/c/iswenzz' color="primary" style={{ margin: 20 }}>
                         <FontAwesomeIcon color='#FF0000' icon={faYoutube} size='2x' />
                     </Fab>
                     <Tooltip disableFocusListener disableTouchListener title="Iswenzz#3906">
-                        <Fab color="primary" style={{ margin: 20 }} aria-label="add">
+                        <Fab color="primary" style={{ margin: 20 }}>
                             <FontAwesomeIcon color='#7289da' icon={faDiscord} size='2x' />
                         </Fab>
                     </Tooltip>
@@ -38,7 +38,7 @@ const Footer: FunctionComponent = (): JSX.Element =>
             bgImage={require(`../../../assets/images/index/${isDarkMode ? 'stars' : 'clouds'}.svg`)}>
                 <Spacing height='200px' />
             </Parallax>
-        </div>
+        </>
     );
 }
 
