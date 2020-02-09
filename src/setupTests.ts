f91541dfe3e39f59bd6d9fda116c6f9ed51f9c26
iswenzz <suxlolz1528@gmail.com>
2020-02-09 21:00:25 +0100

    [Jest] Added setupTests.ts

---
+++ src/setupTests.ts
@@ -0,0 +1,19 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+Object.defineProperty(window, 'matchMedia', 
+{
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
