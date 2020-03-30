ae988f5f66c781bceecc48b4c91e04841e93e2d2
iswenzz <suxlolz1528@gmail.com>
2020-03-30 15:06:08 +0200

    Home: toggle isPastIntro

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,10 @@
 import * as actions from './store/actions';
+import * as appActions from '../../store/actions';
 import React, { FunctionComponent } from 'react';
 import { AppState } from "../../application";
 import { ThunkDispatch } from "redux-thunk";
 import { bindActionCreators } from "redux";
-import { connect, useSelector } from 'react-redux';
-
+import { connect, useSelector, useDispatch } from 'react-redux';
 import NavBar from '../UI/NavBar/NavBar';
 import Projects from './UI/Projects/Projects';
 import Footer from '../UI/Footer/Footer';
@@ -18,8 +18,9 @@ import { Parallax } from 'react-parallax';
 import { Typography } from '@material-ui/core';
 import SplitText from 'react-pose-text';
 import Spacing from '../../components/Spacing/Spacing';
-import '../../Text.scss';
+import VisibilitySensor from 'react-visibility-sensor';
 import ProjectPopup from './UI/ProjectPopup/ProjectPopup';
+import '../../Text.scss';
 
 const charPoses = {
     exit: { opacity: 0, y: 20 },
@@ -32,20 +33,34 @@ const charPoses = {
 
 const Home: FunctionComponent = (): JSX.Element =>
 {
+    const dispatch = useDispatch();
     const isDarkMode = useSelector((state: AppState) => state.app.isDarkMode);
+    const isPastIntro = useSelector((state: AppState) => state.app.isPastIntro);
 
     return (
         <>
-            <NavBar />
-            <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
-            bgImage={require(`../../assets/images/index/${isDarkMode ? '20.jpg' : 'nature1.jpg'}`)} 
-            bgImageAlt="index" strength={400} blur={0}>
-                <Spacing height='70px' />
-                <Intro />
-                <Spacing height='300px' />
-            </Parallax>
-            <IntroSkill /> 
+            {/* Header */}
+            <NavBar style={{ background: 'rgba(50, 50, 60, 0.3)'}} />
 
+            {/* Intro */}
+            <VisibilitySensor partialVisibility>
+            {({ isVisible }) => {
+                if (isVisible === isPastIntro)
+                    dispatch(appActions.togglePastIntro(!isPastIntro));
+                return (
+                    <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
+                    bgImage={require(`../../assets/images/index/${isDarkMode ? '20.jpg' : 'nature1.jpg'}`)} 
+                    bgImageAlt="index" strength={400} blur={0}>
+                        <Spacing height='70px' />
+                        <Intro />
+                        <Spacing height='300px' />
+                    </Parallax>
+                );
+            }}
+            </VisibilitySensor>
+            <IntroSkill />
+
+            {/* Projects */}
             <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
             bgImage={require(`../../assets/images/index/${isDarkMode ? '2.jpg' : '2.jpg'}`)} 
             bgImageAlt="index" strength={-200} blur={0}>
@@ -60,6 +75,7 @@ const Home: FunctionComponent = (): JSX.Element =>
             <ProjectPopup />
             <Projects />
 
+            {/* Levels */}
             <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
             bgImage={require(`../../assets/images/index/${isDarkMode ? '6.jpg' : '20.jpg'}`)} 
             bgImageAlt="index" strength={-200} blur={0}>
@@ -73,6 +89,7 @@ const Home: FunctionComponent = (): JSX.Element =>
             </Parallax>
             <Levels />
 
+            {/* Contact */}
             <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
             bgImage={require(`../../assets/images/index/${isDarkMode ? '55.jpg' : 't1.jpg'}`)} 
             bgImageAlt="index" strength={200} blur={0}>
@@ -85,6 +102,8 @@ const Home: FunctionComponent = (): JSX.Element =>
                 <Spacing height='200px' />
             </Parallax>
             <Contact />
+
+            {/* Footer */}
             <Footer />
         </>
     );
