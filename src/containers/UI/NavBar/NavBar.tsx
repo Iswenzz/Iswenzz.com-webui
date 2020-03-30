d02ed35b419e386e0251e1455738283607b59ff0
iswenzz <suxlolz1528@gmail.com>
2020-03-30 21:01:35 +0200

    NavBar: removed unnecessary grid item

--- src/containers/UI/NavBar/NavBar.tsx
+++ src/containers/UI/NavBar/NavBar.tsx
@@ -147,8 +147,7 @@ export const NavBar: FunctionComponent<NavBarProps> = (props: NavBarProps): JSX.
 							Iswenzz
 						</Typography>
 					</Grid>
-					<Grid item xs={2} />
-					<Grid item xs={7}>
+					<Grid item xs={9}>
 						{isTabletOrMobileDevice || isPortrait ? navBarButtonsMobile : navBarButtonsDesktop}
 					</Grid>
 				</Grid>
