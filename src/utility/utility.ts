4c07edb81be53adc309dacf7c719eccb764f25e8
iswenzz <suxlolz1528@gmail.com>
2020-05-28 01:54:26 +0200

    utility: Added xPath utility function

--- src/utility/utility.ts
+++ src/utility/utility.ts
@@ -1,6 +1,35 @@
 export const updateObject = (oldObject: Object, updatedProperties: Object): any => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    };
+	return {
+		...oldObject,
+		...updatedProperties
+	};
 };
+
+export const getElementsByXPath = (xpath: string, contextNode?: Document) =>
+{
+	let xpathResult = null;
+	if(contextNode === undefined)
+		xpathResult = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+	else
+		xpathResult = contextNode.evaluate(xpath, contextNode, null, XPathResult.ANY_TYPE, null);
+
+	let array = [];
+	let element;
+	element = xpathResult.iterateNext();
+	while(element)
+	{
+		array[array.length] = element;
+		element = xpathResult.iterateNext();
+	}
+	return array;
+}
+
+export const getElementByXPath = (xpath: string, contextNode?: Document) =>
+{
+	let xpathResult = null;
+	if (contextNode === undefined)
+		xpathResult = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+	else
+		xpathResult = contextNode.evaluate(xpath, contextNode, null, XPathResult.ANY_TYPE, null);
+	return xpathResult.iterateNext();
+}
