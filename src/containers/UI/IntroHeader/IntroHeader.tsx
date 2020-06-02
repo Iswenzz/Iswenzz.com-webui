74b5dd011c8b83abb35976751136022438b8154e
iswenzz <suxlolz1528@gmail.com>
2020-06-02 11:06:55 +0200

    IntroHeader: viewport height and animated arrow to smooth scroll

--- src/containers/UI/IntroHeader/IntroHeader.tsx
+++ src/containers/UI/IntroHeader/IntroHeader.tsx
@@ -1,54 +1,57 @@
 import React, { FunctionComponent, memo } from 'react';
 import { Parallax } from 'react-parallax';
-import Spacing from 'components/Spacing/Spacing';
 import SplitText from 'react-pose-text';
 import { Grid, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-scroll';
+import './IntroHeader.scss';
 
 const charPoses = {
-    exit: { opacity: 0, y: 20 },
-    enter: {
-        opacity: 1,
-        y: 0,
-        delay: ({ charIndex }: any) => charIndex * 30
-    }
+	exit: { opacity: 0, y: 20 },
+	enter: {
+		opacity: 1,
+		y: 0,
+		delay: ({ charIndex }: any) => charIndex * 30
+	}
 };
 
 export interface IntroHeaderProps
 {
-    title?: string,
-    desc?: string,
-    spaceTop?: string,
-    spaceBottom?: string,
-    bgImage: string,
-    parallaxStrength?: number,
-    parallaxBlur?: number
+	title?: string,
+	desc?: string,
+	spaceTop?: string,
+	spaceBottom?: string,
+	bgImage: string,
+	parallaxStrength?: number,
+	parallaxBlur?: number
 }
 
 const IntroHeader: FunctionComponent<IntroHeaderProps> = (props: IntroHeaderProps): JSX.Element =>
 {
-    return (
-        <Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
-        bgImage={props.bgImage} bgImageAlt="index" blur={props.parallaxBlur || 0}
-        strength={props.parallaxStrength || 400}>
-            <Spacing height={props.spaceTop || '70px'} />
-            <Grid style={{paddingTop: '200px'}} container 
-            direction="column" justify="center" alignItems="center">
-                <Typography align="center" variant="h3" component="div">
-                    <div className='calli-h1' style={{fontWeight: 'bold'}}>
-                        <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
-                            {props.title}
-                        </SplitText>
-                    </div>
-                    <Typography className="poiret-header" align="center" variant="h3" component="h3">
-                        <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
-                            {props.desc}
-                        </SplitText>
-                    </Typography>
-                </Typography>
-            </Grid>
-            <Spacing height={props.spaceBottom || '300px'} />
-        </Parallax>
-    );
+	return (
+		<div style={{height: '100vh'}}>
+			<Parallax style={{boxShadow: '0 0 5px 6px rgba(60, 60, 60, .3)'}} 
+			bgImage={props.bgImage} bgImageAlt="index" blur={props.parallaxBlur || 0}
+			strength={props.parallaxStrength || 400}>
+				<Grid container className="introheader-grid" direction="column" justify="center" alignItems="center">
+					<Typography className="calli-h1" align="center" variant="h3" component="div">
+						<SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+							{props.title}
+						</SplitText>
+					</Typography>
+					<Typography className="poiret-header" align="center" variant="h3" component="div">
+						<SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+							{props.desc}
+						</SplitText>
+					</Typography>
+					<Link to="intro-section" offset={5} smooth>
+						<FontAwesomeIcon icon={faChevronDown} size="3x" className="introheader-arrow" />
+					</Link>
+				</Grid>
+			</Parallax>
+		</div>
+	);
 }
 
 export default memo(IntroHeader);
