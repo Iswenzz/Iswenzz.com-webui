f92d418e2dd377dd48ef724dd128e3f3a5cd6959
Iswenzz <suxlolz1528@gmail.com>
2020-10-15 15:58:07 +0200

    I18n: Added spanish

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,11 @@
-import i18n, {InitOptions} from "i18next";
+import i18n, {InitOptions, Resource} from "i18next";
 import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const resources = {
+const resources: Record<Language, Resource> = {
 	en: { translation: require("assets/locales/english.json") },
-	fr: { translation: require("assets/locales/french.json") }
+	fr: { translation: require("assets/locales/french.json") },
+	es: { translation: require("assets/locales/spanish.json") },
 };
 
 /**
