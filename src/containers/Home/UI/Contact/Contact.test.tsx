2fbf9104b5f719e2bc50e7288e1c4993e5776fb6
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-10-25 20:34:30 +0100

    Context tests

--- src/containers/Home/UI/Contact/Contact.test.tsx
+++ src/containers/Home/UI/Contact/Contact.test.tsx
@@ -5,11 +5,27 @@ import * as redux from "react-redux";
 import { store } from "application";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Button } from "@material-ui/core";
+// mocks
+// import mockAxios from "./__mocks__/axios";
+// import { useMediaQuery } from "./__mocks__/react-responsive";
 
 describe("[Container] <Contact>", () => 
 {
+	it("Portrait mode", () =>
+	{
+		const Context = React.createContext({orientation: "portrait"});
+		mount((
+			<redux.Provider store={store}>
+				<Context.Consumer>
+					{value => <Contact />}
+				</Context.Consumer>
+			</redux.Provider>
+		));
+	});
+
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	let wrapper: ReactWrapper;
+
 
 	beforeEach(() => 
 	{
@@ -20,20 +36,25 @@ describe("[Container] <Contact>", () =>
 		)); 
 	});
 
+
+	it("Mount test", () => {
+		expect(wrapper.find(Contact)).toBeDefined();
+	});
+
 	it("Sending email", () => 
 	{
-		// wrapper.find("input").at(0).simulate("change", {
-		// 	target: { name: "email", value: "Test@gmail.com" },
-		// 	persist: jest.fn()
-		// });
-		// wrapper.find("input").at(1).simulate("change", {
-		// 	target: { name: "subject", value: "Test subject" },
-		// 	persist: jest.fn()
-		// });
-		// wrapper.find("textarea").simulate("change", {
-		// 	target: { name: "message", value: "Test message" },
-		// 	persist: jest.fn()
-		// });
-		// wrapper.find(Button).simulate("submit");
+		wrapper.find("#email").first().simulate("change", {
+			target: { name: "email", value: "Test@gmail.com" },
+			persist: jest.fn()
+		});
+		wrapper.find("#subject").first().simulate("change", {
+			target: { name: "subject", value: "Test subject" },
+			persist: jest.fn()
+		});
+		wrapper.find("#message").first().simulate("change", {
+			target: { name: "message", value: "Test message" },
+			persist: jest.fn()
+		});
+		wrapper.find(Button).simulate("submit");
 	});
 });
