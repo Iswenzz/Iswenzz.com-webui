fae5e2ae0ad0e726c3ea5f5eab97ce75a1b97b89
Iswenzz <alexisnardiello@gmail.com>
2024-08-31 17:59:27 +0200

    Updated content

--- src/pages/Home/components/Projects/config/config.ts
+++ src/pages/Home/components/Projects/config/config.ts
@@ -1,7 +1,6 @@
 import iswenzzJson from "./Iswenzz.json";
 import aionJson from "./AION.json";
 import cod4Json from "./CoD4.json";
-import schoolJson from "./School.json";
 import projectsJson from "./Projects.json";
 
 import type { ProjectSource } from "../components/Project/Project";
@@ -10,6 +9,5 @@ export const projectsSource: ProjectSource[] = [
 	...iswenzzJson,
 	...aionJson,
 	...cod4Json,
-	...projectsJson,
-	...schoolJson
+	...projectsJson
 ];
